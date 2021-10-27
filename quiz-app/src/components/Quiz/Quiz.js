df4da52a2e00a8fdc48c0fc9890609780cb7b97e
Alex Sandridge <asandridge@users.noreply.github.com>
2021-10-26 18:15:33 -0600

    Front End Minimum Viable Product
    A working quiz interface. Minimal styling. Quiz data is hard coded into
    the project.

---
+++ quiz-app/src/components/Quiz/Quiz.js
@@ -0,0 +1,37 @@
+import appStyles from "../../App.module.css";
+import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+
+const Quiz = (props) => {
+
+    const [quizResults, setQuizResults] = useState({});
+
+    const handleChange = (event) => {
+        let results = quizResults;
+        results[event.target.name] = Number(event.target.value);
+        setQuizResults(results);
+    }
+
+    return (
+        <div className={ appStyles.flexContainer }>
+
+            {Object.keys(props.quizData).map(question => (
+                <div key={question}>
+                    <p>{question}</p>
+                    <div>
+                        {props.quizData[question].answers.map((answer, idx) => (
+                            <div key={question+idx}>
+                                <input name={question} type="radio" id={question+idx} value={idx} onChange={handleChange}/>
+                                <label htmlFor={question+idx}>{answer}</label><br/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+
+            <Button onClick={() => {props.onSubmit(quizResults)}} variant="contained" style={{ color: "white", backgroundColor: "#4CAF50", width: "10%" }}>Submit</Button>
+        </div>
+    );
+}
+
+export default Quiz;
